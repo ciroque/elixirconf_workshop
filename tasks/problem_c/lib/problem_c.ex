30a93e71cd774a164dca222d19f99e2d1a9968eb
INTULO\Steve <scalawagz@outlook.com>
2017-09-05 09:44:13 -0700

    worked c

--- tasks/problem_c/lib/problem_c.ex
+++ tasks/problem_c/lib/problem_c.ex
@@ -6,10 +6,17 @@ defmodule ProblemC do
   def get(timeout) do
     task = Task.async(fn -> slow_request() end)
     # only change below
-    Task.await(timeout, timeout)
+    case Task.yield(task, timeout) do
+      nil -> :request_timeout
+      result -> result
+    end
   end
 
   defp slow_request() do
+#    HTTPoison.get("https://google.com")
     HTTPoison.get("https://bbc.com")
   end
 end
+
+
+// ssl.format
